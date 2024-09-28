1916540cb4a7ccb624f82301699208460f3ce8c5
Atilla Saadat <atillaabc@gmail.com>
2024-09-28 01:33:37 -0700

    test bump verison to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-polaris",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="polaris"),
     install_requires=[
         # Add any dependencies here, e.g., requests
